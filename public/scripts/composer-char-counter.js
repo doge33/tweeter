c53408d2cf8443acaec529ce46197c5ed9518e74
doge33 <sandylx33@gmail.com>
2020-09-09 00:51:49 +0000

    implemented tweet-container html and css(including hover state)

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+//jQuerry; makes sure this won't be triggered until the whole page/DOM is loaded. 
+$(document).ready(function() {
+
+
+  $('textarea').on('keyup', function() {
+
+    let count = $(this).val().length;
+    let remainder = 140 - count;
+    let counter = $(this).siblings().find('.counter').text(remainder);
+
+    $(counter).css({color: (remainder >= 0) ? '' : 'red'})
+});
+})
